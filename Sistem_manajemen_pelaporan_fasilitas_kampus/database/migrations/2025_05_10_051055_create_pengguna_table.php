45c2fdc86072cafb81fa24fbe4c220931ce6cd6f
Fahrizp24 <Fpradian24@gmail.com>
2025-05-20 12:21:21 +0700

    nambah username

--- Sistem_manajemen_pelaporan_fasilitas_kampus/database/migrations/2025_05_10_051055_create_pengguna_table.php
+++ Sistem_manajemen_pelaporan_fasilitas_kampus/database/migrations/2025_05_10_051055_create_pengguna_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('pengguna', function (Blueprint $table) {
             $table->id('pengguna_id');
+            $table->string('username')->unique();
             $table->string('nama');
             $table->string('email')->unique();
             $table->string('kata_sandi');
