0476608fce73cba727310ff2a0708d7d09c72f49
hanoeyf <144418011+hanoeyf@users.noreply.github.com>
2025-05-26 19:05:09 +0700

    perbaikan sarpras

--- Sistem_manajemen_pelaporan_fasilitas_kampus/resources/views/sarpras/sistem_pendukung_keputusan.blade.php
+++ Sistem_manajemen_pelaporan_fasilitas_kampus/resources/views/sarpras/sistem_pendukung_keputusan.blade.php
@@ -8,20 +8,7 @@
             <div class="table-responsive">
                 <div id="table1_wrapper" class="dataTables_wrapper dt-bootstrap5 no-footer">
                     <div class="row">
-                        <div class="col-sm-12 col-md-6">
-                            <div class="dataTables_length" id="table1_length"><label>Show <select name="table1_length"
-                                        aria-controls="table1" class="form-select form-select-sm">
-                                        <option value="10">10</option>
-                                        <option value="25">25</option>
-                                        <option value="50">50</option>
-                                        <option value="100">100</option>
-                                    </select> entries</label></div>
-                        </div>
-                        <div class="col-sm-12 col-md-6">
-                            <div id="table1_filter" class="dataTables_filter"><label>Search:<input type="search"
-                                        class="form-control form-control-sm" placeholder=""
-                                        aria-controls="table1"></label></div>
-                        </div>
+                       
                     </div>
                     <div class="row dt-row">
                         <div class="col-sm-12">
